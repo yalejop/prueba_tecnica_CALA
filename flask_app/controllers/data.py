dbfd1dc7836a11fbaf74c153864bf6cfdd7696cc
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-10-06 11:52:09 -0500

    dar estilo a la gráfica y mejorar el tamaño

--- flask_app/controllers/data.py
+++ flask_app/controllers/data.py
@@ -39,7 +39,7 @@ def pedidos():
 
     print(final_table)
 
-    return render_template('pedidos.html', final_table=[final_table], titles=[''])
+    return render_template('pedidos.html', final_table=[final_table], titles=['NOMBRE'])
 
 
 @app.route('/analisis/')
@@ -60,14 +60,14 @@ def plot():
     table['Edad'] = (((table['Fecha'] - table['NACIMIENTO']).dt.days)/ 365.2425).astype(int)
     cols_to_subset = ['NOMBRE', 'APELLIDO', 'CEDULA', 'NACIMIENTO', 'Nombre_Completo', 'Edad', 'Tipo de pedido', 'numero de pedido']
     clean_table = table[cols_to_subset]
-    total = clean_table.groupby(['Nombre_Completo', 'Tipo de pedido'])['numero de pedido'].count()
+    total = clean_table.groupby(['NOMBRE', 'Tipo de pedido'])['numero de pedido'].count()
 
     img = BytesIO()
     # y = [1,2,3,4,5]
     # x = [0,2,1,3,4]
 
     # plt.plot(total)
-    total.plot(kind='bar')
+    total.plot(kind='bar',figsize=(13,16))
     
     
     plt.show()
